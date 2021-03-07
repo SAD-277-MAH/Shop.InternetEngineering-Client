581e17f000689c2c76d49209bb4491fded88036b
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-03-08 00:16:15 +0330

    Add Category Item

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-$('.div-category').mouseover(function () { 
+$('.div-category').mouseover(function () {
     $('.div-category-list').show();
 });
 
-$('.div-category-list').mouseover(function () { 
+$('.div-category-list').mouseover(function () {
     $('.div-category-list').show();
 });
 
-$('.div-category-list').mouseout(function () { 
+$('.div-category-list').mouseout(function () {
     $('.div-category-list').hide();
 });
 
