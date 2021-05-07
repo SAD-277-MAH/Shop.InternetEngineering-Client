02895e0d7d85043a0c6ae6175e9f16d9179d64f7
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-05-07 22:54:35 +0430

    add pages

--- js/script.js
+++ js/script.js
@@ -3,6 +3,36 @@ function ToggleMenu() {
     togglemenu.classList.toggle('active')
 }
 
+
+function hidefunc(){
+    document.getElementById("accget").style.display = "none";
+    document.getElementById("accedit").style.display = "inline";
+}
+
+function editconfirm() {
+    document.getElementById("accget").style.display = "inline";
+    document.getElementById("accedit").style.display = "none";
+    window.location.reload();
+}
+
+function backfunc(){
+    document.getElementById("accget").style.display = "inline";
+    document.getElementById("accedit").style.display = "none";
+}
+
+function deleteform(){
+    document.getElementById("accget").style.display = "none";
+    document.getElementById("accedit").style.display = "none";
+    document.getElementById("deleteform").style.display = "inline";
+}
+
+function detailform(){
+    document.getElementById("accget").style.display = "inline";
+    document.getElementById("accedit").style.display = "none";
+    document.getElementById("deleteform").style.display = "none";
+}
+
+
 $(document).ready(function () {
 
     $('.div-category').mouseover(function () {
@@ -161,6 +191,10 @@ $(document).ready(function () {
         $("a.controlnp_prev").click(function (e) {
             moveLeftNP();
         });
+
+
+        $(".js-range-slider").ionRangeSlider();
+
     
 
 });
